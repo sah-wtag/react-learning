9e941a64ecdda06487fd480d18a4e7cadef3e469
Md Sadman Ahmed <164001931+sah-wtag@users.noreply.github.com>
2025-05-12 02:24:27 +0600

    Creating Multiple States In React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,17 +2,30 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(1);
   function incrementClick() {
-    setCount(count + 1);
+    setCount(count + incrementBy);
   }
   function decremenClick() {
-    setCount(count - 1);
+    setCount(count - incrementBy);
   }
+
+  function incrementByClick() {
+    setIncrementBy(incrementBy + 1);
+  }
+
+  function decrementByClick() {
+    setIncrementBy(incrementBy - 1);
+  }
+
   return (
-    <h1>
-      <div>Count value is: {count} </div>
+    <div>
+      <h1>Count value is: {count}</h1>
       <button onClick={incrementClick}>Increment</button>
       <button onClick={decremenClick}>Decrement</button>
-    </h1>
+      <h1>We are Incrementing the value by: {incrementBy}</h1>
+      <button onClick={incrementByClick}>Increase Increment</button>
+      <button onClick={decrementByClick}>Decrease Increment</button>
+    </div>
   );
 }
